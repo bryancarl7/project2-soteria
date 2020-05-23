264abaa391e3cad5d8959ae2a451bfd0b0cc3cda
Carter Perkins <cartersperkins@gmail.com>
2020-05-23 16:51:59 -0700

    test cases for web page routes for flask api, fixed busy times reporter bugs

--- data/busy_times/manager.py
+++ data/busy_times/manager.py
@@ -3,7 +3,7 @@ manager.py
 
 ===============================================================================
 
-Last Modified: 22 May 2020
+Last Modified: 23 May 2020
 Modification By: Carter Perkins
 
 Creation Date: 22 May 2020
@@ -16,7 +16,7 @@ the simulated data. Although independent, this component should only interface
 with the driver of the larger module found in 'reporter.py'.
 """
 
-# Standard Import
+# Standard Imports
 import csv
 import os
 
@@ -26,7 +26,7 @@ class SimulationManager():
     """
 
     # Setup data file paths
-    path = "data/busy_times/simulated_data/"
+    path = "/simulated_data/"
     files = (path + "food.csv", path + "grocery.csv", path + "parks.csv",
              path + "store.csv")
 
@@ -46,28 +46,36 @@ class SimulationManager():
             FileNotFoundError               - Missing simulated data file(s)
         """
 
-        valid_location_types = ("temporary": cls.files[0])
+        # https://developers.google.com/places/supported_types
+        # TODO: Smart type to simulated filter
+        valid_location_types = {"restaurant": cls.files[0],
+                                "store": cls.files[3],
+                                "supermarket": cls.files[1],
+                                "park": cls.files[2]}
 
         # Validate input
         if location_type not in valid_location_types:
             raise ValueError("argument 'location_type' is invalid")
 
         # Verify files exist
-        for f in files:
-            if not os.path.isfile(f):
-                raise FileNotFoundError(f)
+        BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+        for dataset in cls.files:
+            if not os.path.isfile(f"{BASE_DIR}{dataset}"):
+                raise FileNotFoundError(f"{BASE_DIR}{dataset}")
 
-        DAYS = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-                "Saturday", "Sunday")
+        DAYS = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
         result = dict.fromkeys(DAYS)
-        with open(cls.files[location_type]) as csvfile:
+
+        # Read simulated data
+        path = BASE_DIR + valid_location_types[location_type]
+        with open(path) as csvfile:
             csvreader = csv.reader(csvfile, delimiter=",")
             for row in csvreader:
                 try:
                     if row[0] not in DAYS:
-                        raise TypeError(f"malformed {cls.files[location_type]}")
+                        raise TypeError(f"malformed {path}")
                     result[row[0]] = list(map(int, row[1:]))
                 except ValueError:
-                    raise TypeError(f"malformed {cls.files[location_type]}")
+                    raise TypeError(f"malformed {path}")
 
         return result
