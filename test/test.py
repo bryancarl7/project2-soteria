32e73046017da15a4e9a74038f3db6a393c39cb3
Carter Perkins <cartersperkins@gmail.com>
2020-05-25 15:41:08 -0700

    more test cases and README init

--- test/test.py
+++ test/test.py
@@ -33,7 +33,9 @@ class ServerTests(unittest.TestCase):
         """
         Tests that all the web pages are accessible.
         """
-        print("\nTesting that web pages are accessible...")
+        print()
+        print("=" * 80)
+        print("Testing that all web pages are accessible...")
         routes = ("/", "/scheduler", "/times")
         # Use Flask test client
         with app.test_client() as client:
@@ -42,18 +44,22 @@ class ServerTests(unittest.TestCase):
                 response = client.get(route)
                 self.assertEqual(response.status_code, 200)
 
+    def test_server_resources(self):
+        """
+        Test server resources.
+        """
+        print()
+        print("=" * 80)
+        print("Testing that all web resources are accessible...")
+        routes = ("/times/bestTime", "/times/bestPlace", "/scheduler/update")
+        with app.test_client() as client:
+            for route in routes:
+                print(f"\tChecking resource {route} via HTTP Get")
+                response = client.get(route)
+                self.assertEqual(response.status_code, 200)
+
 # Need to discuss:
 # TODO: use unittest.mock when using requests OR Flask app.test_client()
-
-class MyTestCase(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        print("test start\n")
-        # app.run(HOST, PORT, debug=True)
-        # api.add_resource(bestTime, "/times/bestTime")
-        # api.add_resource(bestPlace, "/times/bestPlace")
-        # api.add_resource(scheduler, "/scheduler/update")
 
 class BusyTimesReporterTests(unittest.TestCase):
     pass
@@ -69,7 +75,6 @@ class SimulationManagerTests(unittest.TestCase):
         bP = bestPlace(apiHandler)
         parsed = bP.make_request()
         self.assertIsNotNone(parsed)
-    """
     @classmethod
     def test_apiBestPlace(cls):
         response_place = requests.get("http://0.0.0.0:3000/times/bestPlace")
@@ -89,16 +94,16 @@ class SimulationManagerTests(unittest.TestCase):
         response_scheduler = requests.get("http://0.0.0.0:3000/scheduler/update")
         assert(response_scheduler.status_code, 200)
         print("scheduler page successfully created")
-
-    def tearDown(self):
-        print("test end")
+    """
 
     def test_simulated_data(self):
         """
         Tests proper reading of simulated data.
         """
-        print("\nTesting proper reading of simulated data...")
-        cases = ("park", "store") # TODO: , "supermarket", "restaurant")
+        print()
+        print("=" * 80)
+        print("Testing that simulated data sets are accessible...")
+        cases = ("park", "store", "supermarket", "restaurant")
         expected_results = [
             {'Mon': [0, 0, 0, 0, 0, 0, 1, 4, 25, 31, 34, 42, 49, 50, 56, 61,
                      55, 37, 21, 11, 5, 2, 0, 0],
@@ -129,11 +134,43 @@ class SimulationManagerTests(unittest.TestCase):
                      80, 72, 63, 34, 19, 0, 0, 0],
              'Sun': [0, 0, 0, 0, 0, 0, 0, 9, 17, 27, 53, 72, 85, 89, 88, 87,
                      82, 71, 56, 32, 20, 0, 0, 0]
-             }]
+             },
+            {'Mon': [0, 0, 0, 0, 0, 0, 4, 9, 16, 24, 48, 57, 61, 60, 59, 65,
+                     75, 78, 67, 46, 26, 17, 0, 0],
+             'Tue': [0, 0, 0, 0, 0, 0, 7, 14, 21, 38, 48, 59, 67, 70, 67, 65,
+                     69, 72, 67, 52, 29, 16, 0, 0],
+             'Wed': [0, 0, 0, 0, 0, 0, 4, 8, 14, 21, 39, 47, 52, 53, 52, 52,
+                     56, 57, 51, 40, 26, 19, 0, 0],
+             'Thu': [0, 0, 0, 0, 0, 0, 6, 14, 19, 30, 41, 51, 58, 58, 56, 56,
+                     59, 60, 55, 44, 27, 17, 0, 0],
+             'Fri': [0, 0, 0, 0, 0, 0, 9, 16, 23, 29, 52, 61, 67, 69, 66, 65,
+                     66, 68, 62, 48, 28, 19, 0, 0],
+             'Sat': [0, 0, 0, 0, 0, 0, 4, 9, 17, 40, 58, 72, 80, 83, 83, 84,
+                     88, 84, 67, 45, 26, 21, 0, 0],
+             'Sun': [0, 0, 0, 0, 0, 0, 5, 12, 21, 31, 55, 70, 82, 91, 95, 98,
+                     97, 83, 60, 32, 21, 12, 0, 0]
+             },
+            {'Mon': [12, 0, 0, 0, 0, 0, 0, 0, 4, 8, 16, 31, 40, 44, 28, 26, 27,
+                     33, 41, 43, 36, 19, 17, 13],
+             'Tue': [7, 0, 0, 0, 0, 0, 0, 0, 5, 9, 16, 32, 41, 42, 25, 20, 21,
+                     30, 39, 41, 34, 17, 16, 14],
+             'Wed': [9, 0, 0, 0, 0, 0, 0, 0, 5, 8, 18, 33, 40, 36, 17, 15, 19,
+                     28, 36, 39, 35, 18, 13, 8],
+             'Thu': [4, 0, 0, 0, 0, 0, 0, 0, 3, 7, 13, 27, 37, 38, 25, 20, 21,
+                     30, 42, 45, 37, 17, 18, 16],
+             'Fri': [10, 0, 0, 0, 0, 0, 0, 0, 6, 10, 16, 32, 43, 46, 31, 29,
+                     32, 42, 52, 55, 52, 23, 21, 20],
+             'Sat': [14, 6, 0, 0, 0, 0, 0, 0, 8, 16, 34, 56, 69, 71, 48, 44,
+                     44, 52, 62, 62, 49, 20, 21, 21],
+             'Sun': [15, 7, 0, 0, 0, 0, 0, 0, 0, 0, 9, 27, 43, 52, 50, 43, 42,
+                     51, 61, 56, 40, 16, 15, 16]
+             }
+            ]
         for case in cases:
             print(f"\tChecking type {case}")
             observed = SimulationManager.get_busy_times(case)
             expected = expected_results[cases.index(case)]
             self.assertEqual(observed, expected)
+
 if __name__ == '__main__':
     unittest.main()
