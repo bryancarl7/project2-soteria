6116e25fe218f47e51f9bb58fff97bb9fb9229cf
Bryan Carl <thewetcouch@Bryans-MacBook-Pro.local>
2020-05-31 14:38:36 -0700

    added descriptive build messages

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$1" == "-run" ] ; then
+  echo "---------------------------------------------------------------"
+	echo "                ...Launching Flask Server..."
+	echo "---------------------------------------------------------------"
+  python3 -m server.api
+fi
+
 if [ "$1" == "-windows"  ] ; then
 	builds/build_windows "$2"
 else
